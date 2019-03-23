3af5f7c966ab62eea5d0d4961576c548ac04f9d5
sisar4 <alessiocecchi2000@yahoo.it>
2019-03-23 07:44:08 +0100

    aggiustamenti vari e inizio login

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 window.onload = function() {
 closeNav();
 loadMenus();
+showPage(1); /*carica pagina iniziale*/
   };
 function loadMenus()
 {
-    document.getElementById('mySidenav').innerHTML = '<img id="loadingMenuNoLog" src="images/loadingDinamycContent.gif" alt="Attendere..." style="  display: block; margin-left: auto; margin-right: auto;">';
+    //document.getElementById('mySidenav').innerHTML = '<img id="loadingMenuNoLog" src="images/loadingDinamycContent.gif" alt="Attendere..." style="  display: block; margin-left: auto; margin-right: auto;">';
+    //document.getElementById('mySidenav').innerHTML = '<div class="loading-container" id="loadingMenuNoLog"><div class="load1" id="loadingMenuNoLog1"><div class="load2" id="loadingMenuNoLog2"><div class="load3" id="loadingMenuNoLog3"></div></div></div></div>';
     fetch("php/index.php?menu=noLogged", { credentials: "same-origin"}).then((r)=> {return r.json();}).then((res)=> {
         var target = document.getElementById('mySidenav');
         res.forEach((x)=>{
@@ -14,7 +16,7 @@ function loadMenus()
             node.appendChild(text);
             target.appendChild(node);
         });
-        document.getElementById("loadingMenuNoLog").remove();
+        document.getElementsById("loadingMenuNoLog").remove();
     });
 }
 
@@ -66,8 +68,17 @@ x.classList.toggle("change");
             counter=false;
             x.classList.toggle("change");
          }else {
-            document.getElementById("mySidenav").style.width = "225px";
-            document.getElementById("main").style.marginLeft = "225px";
+            var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            if(w<600)
+            {
+                document.getElementById("mySidenav").style.width = "100%";
+            }
+            else
+            {
+                document.getElementById("mySidenav").style.width = "225px";
+                document.getElementById("main").style.marginLeft = "225px";
+            }
+
             counter=true;
             x.classList.toggle("change");
          }
@@ -75,20 +86,74 @@ x.classList.toggle("change");
 
         
       }
-      function onloadAccedi(){                                                    //Modificato in data 22/03/2019     Fani
-    document.getElementById("chiSei").style.display = "block";              //-----
-    document.getElementById("logProf").style.display = "none";              //-----
-    document.getElementById("logStudente").style.display = "none";          //-----
-}                                                                           //-----
-                                                                            //-----
-function onclickProf(){                                                     //-----
-    document.getElementById("chiSei").style.display = "none";               //-----
-    document.getElementById("logProf").style.display = "block";             //-----
-    document.getElementById("logStudente").style.display = "none";          //-----
-}                                                                           //-----
-                                                                            //-----
-function onclickStudente(){                                                 //-----
-    document.getElementById("chiSei").style.display = "none";               //-----
-    document.getElementById("logProf").style.display = "none";              //-----
-    document.getElementById("logStudente").style.display = "block";         //-----
+      function onloadAccedi(){
+    document.getElementById("chiSei").style.display = "block";      
+    document.getElementById("logProf").style.display = "none";
+    document.getElementById("logStudente").style.display = "none";
+}                                                                          
+
+function onclickProf(){                                                   
+    document.getElementById("chiSei").style.display = "none";        
+    document.getElementById("logProf").style.display = "block";      
+    document.getElementById("logStudente").style.display = "none";         
+}                                                                           
+
+function onclickStudente(){                                             
+    document.getElementById("chiSei").style.display = "none";           
+    document.getElementById("logProf").style.display = "none";         
+    document.getElementById("logStudente").style.display = "block";   
 } 
+
+function loginSubmit(form)
+{
+    if(form.name=="loginStud")
+    {
+        const data = { 'action': 'login', 'userType': 'stud', 'uName': form["uname"].value, 'uPwd': form["psw"].value };
+        const querystring = encodeQueryData(data);
+        fetch("php/login.php?"+querystring, { credentials: "same-origin"}).then((r)=> {return r.json();}).then((res)=> {
+        if(res!=null && res!="error")
+            setCookie("token", res, 2);
+            //todo errore se fallisce
+        });
+    }
+    else if(form.name=="loginProf")
+    {
+        const data = { 'action': 'login', 'userType': 'prof', 'uName': form["uname"].value, 'uPwd': form["psw"].value };
+        const querystring = encodeQueryData(data);
+        fetch("php/login.php?"+querystring, { credentials: "same-origin"}).then((r)=> {return r.json();}).then((res)=> {
+        if(res!=null && res!="error")
+            setCookie("token", res, 2);
+            //todo errore se fallisce
+        });
+    }
+}
+
+function encodeQueryData(data) {
+    const ret = [];
+    for (let d in data)
+      ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+    return ret.join('&');
+ }
+
+ function setCookie(cname,cvalue,exhours) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exhours*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
--- php/login.php
+++ php/login.php
@@ -1,7 +1,25 @@
 <?php
-include 'dbCon.php';
+session_start();
+include 'sicurissimo.php';
+if(isset($_GET['action']) && $_GET['action']=="login" && isset($_GET['userType']) && isset($_GET['uName']) && isset($_GET['uPsw']))
+{
+    $tok;
+    if($_GET['userType']=="prof")
+    {
+        /*tirare fuori nome, cognome, ruolo,id*/
+    }
+    else if($_GET['userType']=="stud")
+    {
+        /*tirare fuori nome, cognome, ruolo,id*/
+
+    }
+    
+    echo json_encode($tok);
+}
+/*include 'dbCon.php';
 $dbh = new PDO($dsn,$username,$password);
-$stm = $dbh->prepare("")
+$stm = $dbh->prepare("")*/
+
 /*if(isset($_SESSION['userId']) && isset($_SESSION['userName']))
 {
     echo '<li class="NavLi" style="float:right"><a href="SignIn.php?action=logout">Disconetti '.$_SESSION['userName'].'</a></li>';
@@ -10,4 +28,5 @@ else
 {
     echo '<li class="NavLi" style="float:right"><a href="SignIn.php">Accedi</a></li>';
 }*/
+
 ?>
